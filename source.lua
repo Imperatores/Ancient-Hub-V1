9ee46a9995d062faf43c05628e383b1d96696e41
Imperatores <83348878+Imperatores@users.noreply.github.com>
2022-04-24 20:13:28 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,5 +1,5 @@
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("Ancient Hub V1", "Midnight")
+    local Window = Library.CreateLib("Ancient Hub V1", "BloodTheme")
 
     -- MAIN
     local Main = Window:NewTab("Main")
